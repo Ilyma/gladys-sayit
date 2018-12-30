83818bd133029c4e96ba7578e742d8cc41de1232
Raphael M <mora.r@laposte.net>
2018-12-30 13:47:03 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * Gladys Emotions
+ * List of emotions:
+ * EN: 0 => Joyful; 1 => Irritated; 2 => Bored; 3 => Indifferent; 4 => Sarcastic
+ * FR: 0 => Joyeuse; 1 => Irritée; 2 => Ennuyée; 3 => Indifférente; 4 => Sarcastique
+ */
+
+module.exports = function(sails) {
+
+	var init = require('./lib/init.js');
+
+    gladys.on('ready', function(){
+        init().catch(sails.log.warn);
+    });
+
+	return {
+		install : require('./lib/install.js'),
+		sayHello : require('./lib/sayit/sayit.hello.js')
+
+	};
+};
